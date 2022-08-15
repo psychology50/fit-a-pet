391b55f74b324ebde1e37d4b97014705bb782189
JaeSeo Yang <qud1251@gmail.com>
2022-08-15 14:28:51 +0900

    wow

--- frontend/src/apis/axios.js
+++ frontend/src/apis/axios.js
@@ -28,7 +28,7 @@ axiosInstance.interceptors.response.use(
 
         if (
             error.response.status === 401 &&
-            originalRequest.url === baseURL + 'token/refresh/'
+            originalRequest.url === baseURL + 'signin/refresh/'
         ) {
             window.location.href = '/login/';
             return Promise.reject(error);
@@ -49,7 +49,7 @@ axiosInstance.interceptors.response.use(
 
                 if (tokenParts.exp > now) {
                     return axiosInstance
-                        .post('/token/refresh/', { refresh: refreshToken })
+                        .post('/signin/refresh/', { refresh: refreshToken })
                         .then((response) => {
                             localStorage.setItem('access_token', response.data.access);
                             localStorage.setItem('refresh_token', response.data.refresh);
