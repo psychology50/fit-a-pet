c86900e72a459248df532c78ee7e0eb27d53075b
JaeSeo Yang <qud1251@gmail.com>
2022-08-16 16:43:50 +0900

    modify ERD

--- backend/api/pets/views.py
+++ backend/api/pets/views.py
@@ -1,4 +1,3 @@
-from operator import invert
 from django.http import Http404
 from django.db import transaction
 from django.shortcuts import get_object_or_404
@@ -17,12 +16,32 @@ from api.users.serializers import UserListSerializer
 from .serializers import *
 from .permissions import MemberPermission
 
+def create_init_cycle_data(id):
+    return [
+        {
+            "cycle_name": "식사", "is_cycle": True, "pet_id": id,
+            "mon": True, "tue": True, "wed": True, "thu": True, "fri": True, "sat": True, "sun": True,
+            "detail": [
+                {"detail_name": "아침", "time": "08:00"},
+                {"detail_name": "점심", "time": "13:00"},
+                {"detail_name": "저녁", "time": "18:00"},
+            ]
+        },
+        {
+            "cycle_name": "산책", "is_cycle": False, "pet_id": id, "limit_at": 2,
+            "detail": [{"datail_name": None}]
+        },
+        {
+            "cycle_name": "목욕", "is_cycle": False, "pet_id": id, "limit_at": 7,
+            "detail": [{"datail_name": None}]
+        }
+    ]
+
 class PetViewSet(ModelViewSet):
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
 
     def get_permissions(self):
-        # print(self.action)
         if self.action == 'create' or self.action == 'list':
             permission_classes = [IsAuthenticated]
         else:
@@ -43,24 +62,37 @@ class PetViewSet(ModelViewSet):
         with transaction.atomic():
             instance = serializer.save()
             id_list = CustomUser.objects.filter(nickname__in=member).values_list('user_id', flat=True)
-            for id in id_list:
+            for id in id_list: # create Pet
                 s = MemberSerializer(data={"user_id": id, "pet_id": instance.pet_id})
                 s.is_valid(raise_exception=True)
                 s.save()
+                
+            cycle_data = create_init_cycle_data(instance.pet_id)
+            for data in cycle_data: # create Regular Cycle
+                detail_data = data.pop('detail', False)
+                s = CreateCycleSerializer(data=data)
+                s.is_valid(raise_exception=True)
+                s.save()
+                for data2 in detail_data:
+                    data2['cycle_id'] = s.data['cycle_id']
+                    s2 = CreateDetailSerializer(data=data2)
+                    s2.is_valid(raise_exception=True)
+                    s2.save()
 
-        headers = self.get_success_headers(serializer.data)
-        users = CustomUser.objects.filter(nickname__in=member).values('nickname', 'username', 'profile_img')
-        users_s = UserListSerializer(users, many=True)
-        
-        return Response({
-                "pet_info": serializer.data,
-                "member_info": users_s.data, 
-            },status=status.HTTP_201_CREATED, headers=headers)
+        headers = self.get_success_headers(serializer.data)        
+        return Response(status=status.HTTP_201_CREATED, headers=headers)
 
     def list(self, request):
         member = Member.objects.filter(user_id=request.user)
         pet_list = self.queryset.filter(pet_id__in=member.values_list('pet_id')).order_by('pet_id')
         serializer = ListPetSerializer(pet_list, many=True)
+        return Response(serializer.data)
+
+    @action(methods=['GET'], detail=True)
+    def member_list(self, request, *args, **kwargs):
+        pet_id = kwargs.pop('pk', False)
+        pet = self.queryset.get(pet_id=int(pet_id))
+        serializer = MemberSerializer(pet)
         return Response(serializer.data)
 
     @action(methods=['GET'], detail=True)
@@ -93,17 +125,25 @@ class CycleViewSet(ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         try:
-            pet_id = int(kwargs.pop('pet_pk', False))
-            is_cycle = request.data['is_cycle']
-            request.data['pet_id'] = pet_id
+            pet_id = kwargs.pop('pet_pk', False)
+            detail_data = request.data.pop('detail', False)
+            request.data['pet_id'] = int(pet_id)
         except KeyError:
             return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
-        serializer = self.get_serializer(data=request.data)
+        serializer = CreateCycleSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
 
-        return Response(serializer.data)
+        with transaction.atomic():
+            instance = serializer.save()
+            for d in detail_data:
+                d['cycle_id'] = instance.cycle_id
+                s = CreateDetailSerializer(data=d)
+                s.is_valid(raise_exception=True)
+                s.save()
+
+        headers = self.get_success_headers(serializer.data)
+        return Response({}, status=status.HTTP_201_CREATED, headers=headers)
 
     def list(self, request, *args, **kwargs):
         pet_id = kwargs.pop('pet_pk', False)
