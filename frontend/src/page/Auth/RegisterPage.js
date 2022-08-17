9d214b1e3986787d4cee8b5434dc5732dc498c45
jelly <juicy_joji@yu.ac.kr>
2022-08-18 02:11:20 +0900

    PetProfile

--- frontend/src/page/Auth/RegisterPage.js
+++ frontend/src/page/Auth/RegisterPage.js
@@ -1,7 +1,7 @@
 // import React, { useState } from "react";
 // import axiosInstance from '../../apis/axios'
 // import { useNavigate, Link } from 'react-router-dom';
-import SignUpForm from '../../Componets/Auth/SignUpForm';
+import SignUpForm from '../../Componets/Auth/SignupForm';
 
 function SignUp(){
     return (
--- frontend/src/page/Pet/PetProfile.js
+++ frontend/src/page/Pet/PetProfile.js
@@ -3,19 +3,24 @@ import React from "react";
 // import {Link} from 'react-router-dom';
 import PetProfileNav from "../../Componets/Nav/PetProfileNav";
 import PetInfo from "../../Componets/Pet/PetInfo";
-import styled from 'styled-components';
+import PetList from "../../Componets/Pet/PetList";
+import PetProfileTap from "../../Componets/Pet/PetProfileTap";
+// import styled from 'styled-components';
 
 function PetProfile() {
     return (
         <div>
             <PetProfileNav/>
             <PetInfo/>
-            
+            <PetList/>
+            <PetProfileTap/>
         </div>
     );
 }
 export default PetProfile;
 
-const div=styled.div`
 
-`;
+// const div=styled.div`
+//     background-color: #F5F5F5;
+
+// `;
