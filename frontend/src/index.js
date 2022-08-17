f099240bb51529ae1e3a4cc61af1bc0fd06f9bf8
JaeSeo Yang <qud1251@gmail.com>
2022-08-17 15:51:22 +0900

    del frontend

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css';
-import React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-reportWebVitals();
